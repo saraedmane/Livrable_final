f860ac9e82e40262081eb0111bcf425506d8f46f
saraedmane <edmane.sara@gmail.com>
2018-05-22 02:29:46 +0200

    reparer la casse

--- app/src/main/java/com/example/pierre/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/pierre/myapplication/Main2Activity.java
@@ -1,68 +1,44 @@
 package com.example.pierre.myapplication;
-import android.content.Context;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.content.Intent;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.io.File;
+import android.widget.Toast;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import com.example.pierre.myapplication.R.layout.*;
 
 public class Main2Activity extends AppCompatActivity {
-    public static Client yazid= new Client(33.472831, -112.066411);
+    public static Client user= new Client(33.472831, -112.066411);
 
     private Grid earth = new Grid();
 
     private int d = 0;
 
-    private Context context;
-
-
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+            Bundle b = getIntent().getExtras();
+            String message = b.getString("distance");
+            String mess = b.getString("category");
 
+            d = Integer.parseInt(message);
+            lecture(mess);
 
+            ArrayList<Restaurant> res = earth.listof(user, d);
 
-        Bundle b = getIntent().getExtras();
-        String message = b.getString("distance");
-        String mess=b.getString("category");
-
-
-        d = Integer.parseInt(message);
-
-
-        lecture(mess);
-        ArrayList<Restaurant> res = earth.listof(yazid, d);
-        if (mess!=null) {
-
+            if (res.size() == 0) {
+                Toast.makeText(Main2Activity.this, "No restaurant matches your research", Toast.LENGTH_SHORT).show();
+            } else {
+                MyAdapter myAdapter = new MyAdapter(d);
+                myAdapter.setRestos(res);
+                final RecyclerView rv = (RecyclerView) findViewById(R.id.list);
+                rv.setLayoutManager(new LinearLayoutManager(this));
+                rv.setAdapter(myAdapter);
+            }
         }
-
-        MyAdapter myAdapter = new MyAdapter(d);
-
-        myAdapter.setRestos(res);
-
-        final RecyclerView rv = (RecyclerView) findViewById(R.id.list);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.setAdapter(myAdapter);
-
-
-    }
 
 
 
@@ -77,18 +53,23 @@ public class Main2Activity extends AppCompatActivity {
             e.printStackTrace();
         }
         earth.makeBoxes();
-
-        Log.i("Nombre de service: ", String.valueOf(+services.size()));
         int i = 0;
         while (i < services.size()) {
             Restaurant r = services.get(i);
             if (keyword != null) {
-                Log.i("macategorie: ", String.valueOf(r.category(r.categories)));
-                if (r.category(r.categories).contains(keyword)){
+                if (r.category(Lowercase(r.categories)).contains(keyword)){
                     earth.addRestaurant(r);}}
             else {
                 earth.addRestaurant(r);}
             i++;
         }
+    }
+
+    public static ArrayList<String> Lowercase(ArrayList<String> categories) {
+        ArrayList<String> newcat=new ArrayList<String>();
+        for (String key: categories) {
+            newcat.add(key.toLowerCase());
+        }
+        return newcat;
     }
 }
